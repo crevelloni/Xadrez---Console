8b1f8aee970f8822039ce8b86330b7941fd18b0d
Thiago Crevelloni <thiagocrevelloni@gmail.com>
2023-01-07 20:37:59 -0300

    melhorando a impressão do tabuleiro

--- Program.cs
+++ Program.cs
@@ -12,14 +12,19 @@ namespace Xadrez___Console
             {
                 TabuleiroClass tab = new(8, 8);
 
-                //tab.ColocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
-                //tab.ColocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
-                //tab.ColocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
 
-                //Tela.ImprimirTabuleiro(tab);
 
-                PosicaoXadrez px = new('a', 1);
-                Console.WriteLine(px);
+                tab.ColocarPeca(new Torre(tab, Cor.Branco), new Posicao(5, 1));
+                tab.ColocarPeca(new Torre(tab, Cor.Branco), new Posicao(6, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Branco), new Posicao(7, 0));
+
+                Tela.ImprimirTabuleiro(tab);
+
+                //PosicaoXadrez px = new('a', 1);
+                //Console.WriteLine(px);
 
 
             }
--- Tela.cs
+++ Tela.cs
@@ -12,26 +12,47 @@ namespace Xadrez___Console
 
         public static void ImprimirTabuleiro(TabuleiroClass tab)
         {
+            string[] letras = { " a", "b", "c", "d", "e", "f", "g", "h" };
 
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
+
                 for (int j = 0; j < tab.Colunas; j++)
                 {
+
                     if (tab.RecuperaPeca(i, j) == null)
                     {
                         Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(tab.RecuperaPeca(i,j) + " ");
+                        ImprimirPeca(tab.RecuperaPeca(i, j));
+                        Console.Write(" ");
                     }
 
                 }
                 Console.WriteLine();
-
             }
+            Console.WriteLine("  a b c d e f g h");
 
         }
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branco)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+        }
+
+
 
     }
 }
